32b9c4768bde7edc2a78bdc5e63fc3b69929dc22
agesinardigital <age.ab99@gmail.com>
2023-07-30 11:50:38 +0700

    Users Screen

--- app/users/layout.tsx
+++ app/users/layout.tsx
@@ -1,13 +1,18 @@
+import getUsers from "../actions/getUsers";
 import Sidebar from "../components/Sidebar/Sidebar";
+import UserList from "./components/UserList";
 
-export default function UsersLayout({
+export default async function UsersLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const users = await getUsers();
   return (
     <Sidebar>
-      <div className="h-full">{children}</div>
+      <div className="h-full">
+        <UserList items={users} />
+        {children}</div>
     </Sidebar>
   );
 }
